b8036298d10090a6c99d4341ddb547731f3f5676
wdeveloper16 <wdeveloper16@protonmail.com>
2017-03-13 16:01:46 +0000

    cleanup html and fonts.

---
+++ .tmp/scripts/main.js
@@ -0,0 +1,7 @@
+$(document).ready(function () {
+
+  var wh = $(window).innerHeight();
+  //main content bg image or video height == screen height
+  $('.main-content').css('height', wh);
+});
+//# sourceMappingURL=main.js.map
